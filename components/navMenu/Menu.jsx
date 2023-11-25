be568054d11bb24a8b80232d6c176ccec1c8255e
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-11-25 17:07:16 +0000

    Added menu style classes

--- components/navMenu/Menu.jsx
+++ components/navMenu/Menu.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./styles.module.css";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { IconContext } from "react-icons";
@@ -17,23 +17,16 @@ const Menu = () => {
   return (
     <div>
       <div onClick={toggleMenu}>
-        <AiOutlineMenu size={25} className="cursor-pointer"/>
+        <AiOutlineMenu size={25} className="cursor-pointer" />
       </div>
-      
 
       <div
-        className={`flex justify-center items-center flex-col w-full md:w-[35rem] h-screen bg-[#D7BB8E] fixed top-0 right-0 z-10 transition-transform will-change-transform duration-500 ease-in-out ${
-          menuState.isMenuOpen
-            ? "translate-x-[0]"
-            : "translate-x-[100%]"
+        className={`${styles.menuContainer} ${
+          menuState.isMenuOpen ? "translate-x-[0]" : "translate-x-[100%]"
         }`}
       >
-        <div className="w-full flex justify-between items-center px-[1rem] md:px-[5rem] fixed top-0 py-2 md:py-5">
-          <img
-            className="w-[7.5rem] sm:w-[10rem] md:w-[15rem] lg:w-[12rem]"
-            src="LogoWhite.webp"
-            alt="Logo"
-          />
+        <div className={`${styles.menuLogoContainer}`}>
+          <img className={styles.menuLogo} src="LogoWhite.webp" alt="Logo" />
           <IconContext.Provider
             value={{
               color: "black",
@@ -47,19 +40,11 @@ const Menu = () => {
           </IconContext.Provider>
         </div>
 
-        <ul className="flex justify-center items-center flex-col">
-          <h2 className="hover:cursor-pointer hover:text-[#C69025] transition duration-150 ease-in-out text-[2.25rem] lg:text-[5rem] italic">
-            About
-          </h2>
-          <h2 className="hover:cursor-pointer hover:text-[#C69025] transition duration-150 ease-in-out text-[2.25rem] lg:text-[5rem] italic">
-            Reviews
-          </h2>
-          <h2 className="hover:cursor-pointer hover:text-[#C69025] transition duration-150 ease-in-out text-[2.25rem] lg:text-[5rem] italic">
-            Pricing
-          </h2>
-          <h2 className="hover:cursor-pointer hover:text-[#C69025] transition duration-150 ease-in-out text-[2.25rem] lg:text-[5rem] italic">
-            Contact Us
-          </h2>
+        <ul className={styles.menuList}>
+          <h2 className={styles.menuNavItem}>About</h2>
+          <h2 className={styles.menuNavItem}>Reviews</h2>
+          <h2 className={styles.menuNavItem}>Pricing</h2>
+          <h2 className={styles.menuNavItem}>Contact Us</h2>
         </ul>
       </div>
     </div>

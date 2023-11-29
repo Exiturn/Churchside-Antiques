1de005ad7d0eb39536885d0f5e70e214c201ffff
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-11-29 17:17:27 +0000

    Base of reviewsList component added

--- components/reviews/Reviews.jsx
+++ components/reviews/Reviews.jsx
@@ -1,3 +1,4 @@
+import ReviewsList from "../reviewsList/ReviewsList";
 import styles from "./styles.module.css";
 
 const Reviews = () => {
@@ -7,20 +8,12 @@ const Reviews = () => {
         <h1 className={styles.reviewsHeader}>Reviews</h1>
       </div>
 
-      <main className="pt-[13vw] text-[#C69025] flex flex-col justify-center items-start w-full">
-        <h2 className="text-[#C69025] text-[4.5vh] md:text-[3.5vw]">
+      <main className="pt-[8vw] md:pt-[13vw] text-[#C69025] flex flex-col justify-center items-start w-full">
+        <h2 className="text-[#C69025] text-[4.5vh] md:text-[3.5vw] bold">
           See what our customers had to say...
         </h2>
 
-        <ul className="w-full overflow-x-auto mt-[5vw]">
-          <li className="w-[30vw]">
-            <span className="text-[#C69025] bold text-[2.5vw]">“</span>
-            <p className="text-[#C69025] text-[20px]">
-               We went to the indoor market and were very impressed with the
-              prices offered and the service provided. <span className="text-inherit bold text-[2.5vw]">”</span>
-            </p>
-          </li>
-        </ul>
+        <ReviewsList />
       </main>
     </div>
   );
---
+++ components/reviewsList/ReviewsList.jsx
@@ -0,0 +1,50 @@
+import styles from './styles.module.css'
+
+const ReviewsList = () => {
+  return (
+    <div className={`flex flex-col justify-start items-center md:flex-row overflow-x-auto mt-[5vw] gap-x-[1.25rem] w-full pb-10 ${styles.list}`}>
+          <li className="w-full min-w-[35vw] flex flex-col justify-center items-end md:items-start text-[#C69025]">
+            <p className="text-inherit text-[18px] w-[70%]">
+              {" "}
+              <span className="text-inherit md:text-[1.5rem] bold">“ </span>
+               We went to the indoor market and were very impressed with the
+              prices offered and the service provided. {" "}
+              <span className="text-inherit bold md:text-[1.5rem]"> ”</span>
+            </p>
+            <h2 className="text-inherit italic md:mt-[2rem]">- John Jones</h2>
+          </li>
+          <li className="w-full min-w-[35vw] flex flex-col justify-center items-end md:items-start text-[#C69025]">
+            <p className="text-inherit text-[18px] w-[70%]">
+              {" "}
+              <span className="text-inherit md:text-[1.5rem] bold">“ </span>
+               We went to the indoor market and were very impressed with the
+              prices offered and the service provided. {" "}
+              <span className="text-inherit bold md:text-[1.5rem]"> ”</span>
+            </p>
+            <h2 className="text-inherit italic md:mt-[2rem]">- John Jones</h2>
+          </li>
+          <li className="w-full min-w-[35vw] flex flex-col justify-center items-end md:items-start text-[#C69025]">
+            <p className="text-inherit text-[18px] w-[70%]">
+              {" "}
+              <span className="text-inherit md:text-[1.5rem] bold">“ </span>
+               We went to the indoor market and were very impressed with the
+              prices offered and the service provided. {" "}
+              <span className="text-inherit bold md:text-[1.5rem]"> ”</span>
+            </p>
+            <h2 className="text-inherit italic md:mt-[2rem]">- John Jones</h2>
+          </li>
+          <li className="w-full min-w-[35vw] flex flex-col justify-center items-end md:items-start text-[#C69025]">
+            <p className="text-inherit text-[18px] w-[70%]">
+              {" "}
+              <span className="text-inherit md:text-[1.5rem] bold">“ </span>
+               We went to the indoor market and were very impressed with the
+              prices offered and the service provided. {" "}
+              <span className="text-inherit bold md:text-[1.5rem]"> ”</span>
+            </p>
+            <h2 className="text-inherit italic md:mt-[2rem]">- John Jones</h2>
+          </li>
+        </div>
+  )
+}
+
+export default ReviewsList

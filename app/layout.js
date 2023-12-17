9a25beb749f8c1879c749f62aad65249d8f51d8c
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-12-17 13:48:10 +0000

    Added analytics component

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,23 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Churchside Antiques',
-  description: ' Antique Buying & Selling based in Nantwich | We buy everything from a silver thimble to a grandfather clock. Just ask!',
-  googleSiteVerification: 'wX0p2TC1gErEQjTVy5s3V419NxOJY9L0kXzq4nOyffE',
-}
+  title: "Churchside Antiques",
+  description:
+    " Antique Buying & Selling based in Nantwich | We buy everything from a silver thimble to a grandfather clock. Just ask!",
+  googleSiteVerification: "wX0p2TC1gErEQjTVy5s3V419NxOJY9L0kXzq4nOyffE",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
